9d72975207537fbf51e00d37d5e03a83fb6f56a7
Wajid Javed <wajidjaved17@gmail.com>
2019-10-23 23:13:51 +0500

    all done

---
+++ PlaceService.js
@@ -0,0 +1,286 @@
+/**/_xdc_._z7stsf && _xdc_._z7stsf( {
+   "html_attributions" : [],
+   "result" : {
+      "address_components" : [
+         {
+            "long_name" : "Suite 5",
+            "short_name" : "Suite 5",
+            "types" : [ "subpremise" ]
+         },
+         {
+            "long_name" : "501",
+            "short_name" : "501",
+            "types" : [ "street_number" ]
+         },
+         {
+            "long_name" : "West 168th Street",
+            "short_name" : "W 168th St",
+            "types" : [ "route" ]
+         },
+         {
+            "long_name" : "Manhattan",
+            "short_name" : "Manhattan",
+            "types" : [ "sublocality_level_1", "sublocality", "political" ]
+         },
+         {
+            "long_name" : "New York",
+            "short_name" : "New York",
+            "types" : [ "locality", "political" ]
+         },
+         {
+            "long_name" : "New York County",
+            "short_name" : "New York County",
+            "types" : [ "administrative_area_level_2", "political" ]
+         },
+         {
+            "long_name" : "New York",
+            "short_name" : "NY",
+            "types" : [ "administrative_area_level_1", "political" ]
+         },
+         {
+            "long_name" : "United States",
+            "short_name" : "US",
+            "types" : [ "country", "political" ]
+         },
+         {
+            "long_name" : "10032",
+            "short_name" : "10032",
+            "types" : [ "postal_code" ]
+         }
+      ],
+      "adr_address" : "\u003cspan class=\"street-address\"\u003e501 W 168th St Suite 5\u003c/span\u003e, \u003cspan class=\"locality\"\u003eNew York\u003c/span\u003e, \u003cspan class=\"region\"\u003eNY\u003c/span\u003e \u003cspan class=\"postal-code\"\u003e10032\u003c/span\u003e, \u003cspan class=\"country-name\"\u003eUSA\u003c/span\u003e",
+      "formatted_address" : "501 W 168th St Suite 5, New York, NY 10032, USA",
+      "formatted_phone_number" : "(347) 620-6226",
+      "geometry" : {
+         "location" : {
+            "lat" : 40.84018400000001,
+            "lng" : -73.9372631
+         },
+         "viewport" : {
+            "northeast" : {
+               "lat" : 40.8414737802915,
+               "lng" : -73.9359603697085
+            },
+            "southwest" : {
+               "lat" : 40.8387758197085,
+               "lng" : -73.93865833029152
+            }
+         }
+      },
+      "icon" : "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+      "id" : "b44af1043209d8580986e350c1bed12373cf1b89",
+      "international_phone_number" : "+1 347-620-6226",
+      "name" : "Health Aide Inc.",
+      "opening_hours" : {
+         "open_now" : true,
+         "periods" : [
+            {
+               "close" : {
+                  "day" : 1,
+                  "time" : "1700"
+               },
+               "open" : {
+                  "day" : 1,
+                  "time" : "0900"
+               }
+            },
+            {
+               "close" : {
+                  "day" : 2,
+                  "time" : "1700"
+               },
+               "open" : {
+                  "day" : 2,
+                  "time" : "0900"
+               }
+            },
+            {
+               "close" : {
+                  "day" : 3,
+                  "time" : "1700"
+               },
+               "open" : {
+                  "day" : 3,
+                  "time" : "0900"
+               }
+            },
+            {
+               "close" : {
+                  "day" : 4,
+                  "time" : "1700"
+               },
+               "open" : {
+                  "day" : 4,
+                  "time" : "0900"
+               }
+            },
+            {
+               "close" : {
+                  "day" : 5,
+                  "time" : "1300"
+               },
+               "open" : {
+                  "day" : 5,
+                  "time" : "0900"
+               }
+            }
+         ],
+         "weekday_text" : [
+            "Monday: 9:00 AM – 5:00 PM",
+            "Tuesday: 9:00 AM – 5:00 PM",
+            "Wednesday: 9:00 AM – 5:00 PM",
+            "Thursday: 9:00 AM – 5:00 PM",
+            "Friday: 9:00 AM – 1:00 PM",
+            "Saturday: Closed",
+            "Sunday: Closed"
+         ]
+      },
+      "photos" : [
+         {
+            "height" : 1199,
+            "html_attributions" : [
+               "\u003ca href=\"https://maps.google.com/maps/contrib/117519187299252483605/photos\"\u003eHealth Aide Inc.\u003c/a\u003e"
+            ],
+            "photo_reference" : "CmRaAAAA_JSyUEtzlUSdf5KWhKmFaocQSpbKARj48y6vOW-iWNxByYDMgbSSg_dIoasQzYzwdYCENr9xRLP6WCMgm0OTKuOdZasWmgkjUpXksobOmlCOm4nTu1WIjcotZ3TahSzuEhANAeHJdMCMs-W2fAi-0aGQGhSAVRY0PxxInhoZyRxlW0Et3iq82w",
+            "raw_reference" : {
+               "fife_url" : "https://lh3.googleusercontent.com/p/AF1QipOX5bT_bGO6vBQpTtpJ7QCn1YDqm0o01Me4n_a8=k"
+            },
+            "width" : 1800
+         },
+         {
+            "height" : 1207,
+            "html_attributions" : [
+               "\u003ca href=\"https://maps.google.com/maps/contrib/104267715479916886647/photos\"\u003eHealth Aide\u003c/a\u003e"
+            ],
+            "photo_reference" : "CmRaAAAAD0UzJSW6113YowOkh9QwQ9ARgZl3nbteyP3-Dp044PLlMihKJwJWBhcJJVeLRMZNOjiwbEgoUlo4X1jlpQHGJZxvLBZkaZY_N0K8juG0DlhKhs3nMueUGN9IJubIpBRUEhA0nCPlAvLP1zt2NU8lbAxBGhRxM5I1BbC9V9Q9CsFV1PJRYB36Tw",
+            "raw_reference" : {
+               "fife_url" : "https://lh3.googleusercontent.com/p/AF1QipN68SMSFTTGl2jCaUBBwSIYmWAO_Ef26yL1CZXI=k"
+            },
+            "width" : 1850
+         },
+         {
+            "height" : 1082,
+            "html_attributions" : [
+               "\u003ca href=\"https://maps.google.com/maps/contrib/117519187299252483605/photos\"\u003eHealth Aide Inc.\u003c/a\u003e"
+            ],
+            "photo_reference" : "CmRaAAAAjcHhpYEvT5BSSu6GM9V4eVu06G3wDc3BWwBmTUOevTicvlWsKIOP1Gli0abxspVU3YYgAoCRe-cBLxqvMu96BkrXZb21Xte_7rLm6UmpRM37Sdh3q2ZGuPEEt--2yT_MEhB12J92APLq02UNRQHeJd23GhTvxnuPpT4P2LA87aw_b96DksU-9A",
+            "raw_reference" : {
+               "fife_url" : "https://lh3.googleusercontent.com/p/AF1QipOPwndMu_XTcFZhLG_SzhxGc0X4qaYTisswnAW3=k"
+            },
+            "width" : 1655
+         },
+         {
+            "height" : 1929,
+            "html_attributions" : [
+               "\u003ca href=\"https://maps.google.com/maps/contrib/104267715479916886647/photos\"\u003eHealth Aide\u003c/a\u003e"
+            ],
+            "photo_reference" : "CmRaAAAAbdt0bhoRHguPEhpCGCSTHzTTM1y7K4iauG5AfBmnoDsgW9u09C9l4FVVlLppeZtuQ3E6I3cP3eDn11KNmf1oM1coM4ZLkftXEEJO9LYFtEQwhLVdAnWwntQWdJkxHrY_EhB13HphHf0wX5MlFhutSMIfGhTWkcVbGoyYpo3hpmBvqlH-wuU1_w",
+            "raw_reference" : {
+               "fife_url" : "https://lh3.googleusercontent.com/p/AF1QipOaQF3-OdbrRhyoyjS-mgsZRxXQ9IaP5wZnbZ95=k"
+            },
+            "width" : 3413
+         },
+         {
+            "height" : 1467,
+            "html_attributions" : [
+               "\u003ca href=\"https://maps.google.com/maps/contrib/104267715479916886647/photos\"\u003eHealth Aide\u003c/a\u003e"
+            ],
+            "photo_reference" : "CmRaAAAARgttd9tpiFKENh6DRxC5shqNE_8uFJNFtF1mKRbb7_gcj-3Khe-u5VTMoyvoyRWO6oYHWFWRWARKWwkuMo-VcoaZznAjjNmlIxFd2jCVNVjiNizcx1H0pkVhRVmBQAucEhCYFF4CILY6tqyKWM7X3naLGhSVAzj_K5kdlCQsVrkyyBVtNn2qAA",
+            "raw_reference" : {
+               "fife_url" : "https://lh3.googleusercontent.com/p/AF1QipOJxjWU5K-PyUV3BRgmLv7qcoe75qQt4fQMz2nq=k"
+            },
+            "width" : 2200
+         },
+         {
+            "height" : 745,
+            "html_attributions" : [
+               "\u003ca href=\"https://maps.google.com/maps/contrib/112589373128012993095/photos\"\u003eDima Mo\u003c/a\u003e"
+            ],
+            "photo_reference" : "CmRZAAAA-eRnQGFIrY4RKuP0vdUFJv64KnHjdYMJsGLnKmMaM1sJpcnjoHwQvwinGmJ2sandihuMSq6dLBDCWRjF69wFVxVebumCWE3Qn0p2D9k1clhPmuu1gwaFeFaX78tnFmNdEhDDnp0lkzVw65HcVtZ3n8hhGhSSQq9eX7oaknzg-rNkTRgeAjLKsw",
+            "raw_reference" : {
+               "fife_url" : "https://lh3.googleusercontent.com/p/AF1QipMlPy2Rm3iva5aP52QggqLDoJ85xEgLqpSXaQc=k"
+            },
+            "width" : 1646
+         },
+         {
+            "height" : 1199,
+            "html_attributions" : [
+               "\u003ca href=\"https://maps.google.com/maps/contrib/104267715479916886647/photos\"\u003eHealth Aide\u003c/a\u003e"
+            ],
+            "photo_reference" : "CmRaAAAAxiN4Ykz33uZdjEa-MhC1tzygSa8qsMs3QUw4Pebu7G8eBvDq62troTvxsBNRmbVz3JCt783gEXj5sTv1cMdE3qmo7Jih7Tmbz4Yt-4SrliYt8rMcj9Nzyx_eWY9KfhriEhCg7-Na-TUuiSLOOYZBaOfwGhRt3vbD4_TNeNr3C1LN8zvT6PE3dw",
+            "raw_reference" : {
+               "fife_url" : "https://lh3.googleusercontent.com/p/AF1QipOoxzHd4HznaYx6dAap1L99JTSs3sfH_e8eslMa=k"
+            },
+            "width" : 1800
+         }
+      ],
+      "place_id" : "ChIJtemRg_pbwokRKCpQq6jd7Kk",
+      "plus_code" : {
+         "compound_code" : "R3R7+33 New York, United States",
+         "global_code" : "87G8R3R7+33"
+      },
+      "rating" : 4.699999999999999,
+      "reference" : "ChIJtemRg_pbwokRKCpQq6jd7Kk",
+      "reviews" : [
+         {
+            "author_name" : "Brandon Jeffries",
+            "author_url" : "https://www.google.com/maps/contrib/108718623385205423841/reviews",
+            "language" : "en",
+            "profile_photo_url" : "https://lh4.ggpht.com/-c-eZv3CXDjg/AAAAAAAAAAI/AAAAAAAAAAA/SlwjuThu1Fk/s128-c0x00000000-cc-rp-mo/photo.jpg",
+            "rating" : 5,
+            "relative_time_description" : "7 months ago",
+            "text" : "Just as we were talking about the possibility of getting in home care for our mother I heard about Health Aide from a friend. Staff were kind & compassionate about our needs. My mother had caregivers that treated her with love, kindness & dignity. As our needs changed and those of our mother's they met those needs.",
+            "time" : 1553097689
+         },
+         {
+            "author_name" : "Clinton Romero",
+            "author_url" : "https://www.google.com/maps/contrib/116353744531054326027/reviews",
+            "language" : "en",
+            "profile_photo_url" : "https://lh6.ggpht.com/-yAyyDE3MCzI/AAAAAAAAAAI/AAAAAAAAAAA/SgxTZMjEF-A/s128-c0x00000000-cc-rp-mo/photo.jpg",
+            "rating" : 5,
+            "relative_time_description" : "7 months ago",
+            "text" : "I had a wonderful experience with Health Aide. From the first meeting they were attentive, thorough, and responsive to our needs. They took into account all the unique factors that would help my father have the best care and found the perfect persons to aid him. This is a challenge because my dad is not American and has some cultural reservations about being cared for. Luckily, Corewood eased all his worries, and he has since looked forward to visits from the caregiver! Such individualized attention is rare in today's health landscape. I really appreciate all the stress they relieved from our family, and it's been a joy to seem my dad feel more independent and confident. I can't recommend Health Aide enough.",
+            "time" : 1551804869
+         },
+         {
+            "author_name" : "Yolanda Dayanara Colon",
+            "author_url" : "https://www.google.com/maps/contrib/116511289525146646198/reviews",
+            "language" : "en",
+            "profile_photo_url" : "https://lh3.ggpht.com/-xvmq7s6pQQ8/AAAAAAAAAAI/AAAAAAAAAAA/56h3s0OiuXQ/s128-c0x00000000-cc-rp-mo/photo.jpg",
+            "rating" : 5,
+            "relative_time_description" : "9 months ago",
+            "text" : "I have no qualms at highly recommending Health Aide for the care of loved ones in their own homes. My father was under Health Aide tender loving care for about a year and the service was exemplary! We had caretakers that worked so well with my father and figured out how to treat him that they created a solid bond. The home was always clean, appointments always kept, good healthy food given and any money they had to spend for my father was recorded with receipts. When we did have a conflict of character between the caregiver and my father it was remedied within a few days; efficiently, quickly, without drama and for the better of all parties. I live out of state from my dad and knowing that Health Aide had things under control was such a relief for me being miles away and taking care of my own family. My rating for this company is certainly in regards to the people and care they gave.",
+            "time" : 1548085535
+         },
+         {
+            "author_name" : "Marcus Griffith",
+            "author_url" : "https://www.google.com/maps/contrib/101373684870117425961/reviews",
+            "language" : "en",
+            "profile_photo_url" : "https://lh3.ggpht.com/-07VxW3trijM/AAAAAAAAAAI/AAAAAAAAAAA/r_PPiHrkljc/s128-c0x00000000-cc-rp-mo/photo.jpg",
+            "rating" : 5,
+            "relative_time_description" : "7 months ago",
+            "text" : "Health Aide was super responsive and able to start their aide service the same day I called.  The aides are terrific - extremely attentive, kind, and caring.  I highly recommend Health Aide!",
+            "time" : 1552913225
+         },
+         {
+            "author_name" : "Shawn Cobbs",
+            "author_url" : "https://www.google.com/maps/contrib/105067574232005137294/reviews",
+            "language" : "en",
+            "profile_photo_url" : "https://lh6.ggpht.com/-FHyB8waEEI8/AAAAAAAAAAI/AAAAAAAAAAA/t8JjxBFiBl4/s128-c0x00000000-cc-rp-mo/photo.jpg",
+            "rating" : 5,
+            "relative_time_description" : "6 months ago",
+            "text" : "The best home care company I have ever used.  Incredible caring staff that go above and beyond their caregiving responsibilities.",
+            "time" : 1554215241
+         }
+      ],
+      "scope" : "GOOGLE",
+      "types" : [ "health", "point_of_interest", "establishment" ],
+      "url" : "https://maps.google.com/?cid=12244405203411937832",
+      "user_ratings_total" : 26,
+      "utc_offset" : -240,
+      "vicinity" : "501 West 168th Street Suite 5, New York",
+      "website" : "http://www.healthaide.org/"
+   },
+   "status" : "OK"
+}
+ )
